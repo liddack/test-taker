a4d6b288d578bbdc5b1e9632f6f805521df6a6e3
Lucas Miranda <me@liddack.dev>
2024-03-29 22:01:57 +0000

    feat: results page

---
+++ components/exam-result.tsx
@@ -0,0 +1,80 @@
+import { StandaloneQuestion } from "@/classes/standalone-question";
+import { Dispatch, SetStateAction } from "react";
+
+type ExamResultProps = {
+  questions: StandaloneQuestion[];
+  setShowResultsPage: Dispatch<SetStateAction<boolean>>;
+  answers: number[][];
+};
+
+const buttonStyle = `px-3 py-2 mr-2 bg-slate-700 text-white rounded cursor-pointer hover:bg-slate-600 disabled:text-slate-300 disabled:bg-slate-500`;
+const containsAll = (arr1: number[], arr2: number[]) =>
+  arr2.every((arr2Item) => arr1.includes(arr2Item));
+
+const sameMembers = (arr1: number[], arr2: number[]) =>
+  containsAll(arr1, arr2) && containsAll(arr2, arr1);
+
+export default function ExamResult({
+  questions,
+  answers,
+  setShowResultsPage,
+}: ExamResultProps) {
+  const correctAnswers = answers.filter((a, i) => {
+    const questionAnswers = questions[i].answers;
+    return sameMembers(a, questionAnswers);
+  });
+  return (
+    <>
+      <h1 className="text-6xl text-center font-bold mb-4">
+        {Math.round((correctAnswers.length / questions.length) * 100)}%
+      </h1>
+      <h2 className="text-2xl text-center font-bold mb-4">
+        Você acertou {correctAnswers.length} de {questions.length} questões
+      </h2>
+      <p className="text-center mb-6">
+        <button className={buttonStyle} onClick={() => setShowResultsPage(false)}>
+          Voltar
+        </button>
+      </p>
+      <h2 className="font-bold text-2xl mb-3 text-center">Resultados</h2>
+      {questions.map((q, i) => {
+        const isCorrect = sameMembers(q.answers, answers[i]);
+        return (
+          <>
+            <div className="border p-4 border-slate-400 my-2 rounded-md">
+              <h3
+                className={`inline-flex items-center text-normal text-sm font-semibold mb-2 ${
+                  isCorrect ? "text-emerald-700" : "text-red-600"
+                }`}
+              >
+                {isCorrect ? "✅" : "❌"} {q.command}
+              </h3>
+              <div className="ml-6 my-1">
+                {q.alternatives.map((alt, idx) => {
+                  const wasChosen = answers[i].includes(idx);
+                  const isCorrect = q.answers.includes(idx);
+                  const type = q.answers.length > 1 ? "checkbox" : "radio";
+                  return (
+                    <div
+                      key={idx}
+                      className={`py-1 px-2 my-2 text-sm rounded bg-opacity-50 border-opacity-50 border-2 ${
+                        isCorrect
+                          ? "bg-green-100 border-emerald-600"
+                          : "bg-red-100 border-red-600"
+                      }`}
+                    >
+                      <label className={`flex items-center`}>
+                        <input type={type} checked={wasChosen} onClick={() => {}} />
+                        <span className="ml-1">{alt}</span>
+                      </label>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </>
+        );
+      })}
+    </>
+  );
+}
---
+++ components/standalone-exam.tsx
@@ -0,0 +1,121 @@
+"use client";
+
+import { StandaloneQuestion } from "@/classes/standalone-question";
+import { ChangeEvent, Dispatch, ReactNode, SetStateAction, useCallback } from "react";
+
+type StandaloneExamProps = {
+  questions: StandaloneQuestion[];
+  setShowResultsPage: Dispatch<SetStateAction<boolean>>;
+  answers: number[][];
+  setAnswers: Dispatch<SetStateAction<number[][]>>;
+  currentQuestion: number;
+  setCurrentQuestion: Dispatch<SetStateAction<number>>;
+};
+
+export default function StandaloneExam({
+  questions,
+  answers,
+  setAnswers,
+  currentQuestion,
+  setCurrentQuestion,
+  setShowResultsPage,
+}: StandaloneExamProps) {
+  const Main = ({ children }: { children: ReactNode }) => (
+    <main className="flex-start grow flex flex-col justify-center">{children}</main>
+  );
+  const buttonStyle = `px-3 py-2 mr-2 bg-slate-700 text-white rounded cursor-pointer hover:bg-slate-600 disabled:text-slate-300 disabled:bg-slate-500`;
+
+  const q = questions[currentQuestion],
+    hasPrevious = currentQuestion - 1 >= 0,
+    hasNext = currentQuestion + 1 < questions.length;
+
+  const questionCount = (count: number) => (
+    <>
+      {count > 1 ? (
+        <>
+          Esta questão possui <strong>{count}</strong> respostas corretas.
+        </>
+      ) : (
+        `Escolha a resposta correta.`
+      )}
+    </>
+  );
+
+  const storeAnswer = useCallback(
+    (e: ChangeEvent<HTMLInputElement>, question: StandaloneQuestion, index: number) => {
+      const answersCp = [...answers];
+      if (question.answers.length > 1) {
+        // checkbox
+        if (e.currentTarget.checked) {
+          answersCp[currentQuestion].push(index);
+        } else {
+          answersCp[currentQuestion] = answersCp[currentQuestion].filter(
+            (a) => a != index
+          );
+        }
+      } else {
+        // radio
+        answersCp[currentQuestion] = [index];
+      }
+      setAnswers(answersCp);
+      console.log(answersCp);
+    },
+    [answers, currentQuestion, setAnswers]
+  );
+
+  if (!questions.length) return <Main>Erro ao buscar teste.</Main>;
+  return (
+    <>
+      <div className="flex justify-end">
+        {currentQuestion + 1} de {questions.length}
+      </div>
+      <h1 className="text-xl font-bold mb-1">{q.command}</h1>
+      <span className="text-sm">{questionCount(q.answers.length)}</span>
+      <div className="relative pt-4">
+        {q.alternatives.map((a, i) => (
+          <div key={i} className="flex mb-3">
+            <input
+              className="h-4 w-4 mt-[0.2rem] border-gray-300 focus:ring-2 focus:ring-blue-300"
+              type={q.answers.length > 1 ? "checkbox" : "radio"}
+              id={`q:${q.id}_a:${i}`}
+              onChange={(e) => storeAnswer(e, q, i)}
+              checked={answers[currentQuestion].includes(i)}
+            ></input>
+            <label
+              className="ms-2 text font-medium text-gray-90 ml-2 block"
+              htmlFor={`q:${q.id}_a:${i}`}
+            >
+              {a}
+            </label>
+          </div>
+        ))}
+        <div className="flex justify-center mt-6">
+          {hasPrevious && (
+            <button
+              className={buttonStyle}
+              onClick={() => setCurrentQuestion(currentQuestion - 1)}
+            >
+              {"<"} Anterior
+            </button>
+          )}
+          {hasNext && (
+            <button
+              className={buttonStyle}
+              onClick={() => hasNext && setCurrentQuestion(currentQuestion + 1)}
+            >
+              Próxima {">"}
+            </button>
+          )}
+          {hasPrevious && !hasNext && (
+            <button
+              className={buttonStyle + ` font-bold`}
+              onClick={() => setShowResultsPage(true)}
+            >
+              Ver resultado
+            </button>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
