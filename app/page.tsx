fd11545d835d003e4552f923c0b1b39d3d808b83
Lucas Miranda <me@liddack.dev>
2024-03-29 23:17:56 +0000

    feat(home): add link to demo exam page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,22 @@
 "use client";
 import { HomeTests } from "@/components/tests";
+import Link from "next/link";
 import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <main className="flex-start grow flex flex-col justify-center">
+    <main className="flex-start grow flex flex-col justify-center text-center">
       <h1 className="text-xl font-bold">Questionários</h1>
       <div className="relative flex place-items-center flex-col pt-4">
         <Suspense fallback={<p>Carregando...</p>}>
           <HomeTests></HomeTests>
         </Suspense>
+        <Link
+          href="/take/standalone?demo=true"
+          className="underline hover:text-slate-700"
+        >
+          Demonstração de teste
+        </Link>
       </div>
     </main>
   );
