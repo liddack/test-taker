1f46bbd354ba7bfdf519cdc37be5a0a0094b21fc
Lucas Miranda <me@liddack.dev>
2024-04-09 13:19:54 +0000

    feat(ExamResult): add keyboard navigation

--- components/kbd.tsx
+++ components/kbd.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export function Kbd({ children }: { children: ReactNode }) {
   return (
-    <kbd className="bg-slate-800 border-b-slate-900 border-b px-2 py-1 rounded">
+    <kbd className="bg-slate-800 text-white border-b-slate-900 border-b pb-[0.1rem] px-2 py-1 rounded text-[.9em] leading-4">
       {children}
     </kbd>
   );
