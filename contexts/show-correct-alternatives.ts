e8f592c60e4ee0697e7074da3a79f8969195c6bd
Lucas Miranda <me@liddack.dev>
2024-05-06 06:24:02 +0000

    feat: save app `showCorrectAlternatives` in IndexedDB

--- contexts/show-correct-alternatives.ts
+++ contexts/show-correct-alternatives.ts
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, createContext } from "react";
+import { createContext } from "react";
 
 export const ShowCorrectAlternativesContext = createContext([false, () => {}] as [
   boolean,
--- db/db.model.ts
+++ db/db.model.ts
@@ -1,14 +1,34 @@
 import { StandaloneQuestion } from "@/classes/standalone-question";
-import Dexie, { Table } from "dexie";
+import Dexie, { Table, Transaction } from "dexie";
+
+export interface AppSettings {
+  name: string;
+  value: any;
+}
+
+export enum AppSetting {
+  ShowCorrectAlternatives = "showCorrectAlternatives",
+  ShowCorrectAnswersOnly = "showCorrectAnswersOnly",
+  CurrentQuestion = "currentQuestion",
+}
 
 export class DB extends Dexie {
   // table name is student
   questions!: Table<StandaloneQuestion>;
+  settings!: Table<AppSettings>;
   constructor() {
     super("simuladosDatabase");
     this.version(1).stores({
       questions:
         "++id, command, alternatives, answers, checkedAlternatives, questionSetId, authorId", // Primary key and indexed props
+      settings: "++name, value",
+    });
+    this.on("populate", async (tx: Transaction) => {
+      await tx.table("settings").bulkAdd([
+        { name: AppSetting.CurrentQuestion, value: 0 },
+        { name: AppSetting.ShowCorrectAlternatives, value: true },
+        { name: AppSetting.ShowCorrectAnswersOnly, value: false },
+      ]);
     });
   }
 }
