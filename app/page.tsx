9e2d455b286d12283688191b3280b0213fda00be
Lucas Miranda <me@liddack.dev>
2024-05-06 05:52:31 +0000

    fix(app/page): change question existence check source

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,27 @@
 "use client";
-import Link from "next/link";
 import { useEffect } from "react";
-import { readFromLocalStorage } from "./lib/utils/storage";
 import { useRouter } from "next/navigation";
-import { ImportedQuestion } from "@/interfaces/imported-question";
+import { db } from "@/db/db.model";
 
 export default function Home() {
   const router = useRouter();
   useEffect(() => {
-    const questions = readFromLocalStorage("questionsData") as ImportedQuestion[];
-
-    if (!questions?.length) {
-      router.push("/upload-questions");
-    } else {
-      router.push("/take/standalone");
-    }
+    const checkQuestions = async () => {
+      const questions = await db.questions.toArray();
+      if (!questions?.length) {
+        router.push("/upload-questions");
+      } else {
+        router.push("/take/standalone");
+      }
+    };
+    checkQuestions();
   }, [router]);
 
   return (
     <main className="flex-start grow flex flex-col justify-center text-center">
       <h1 className="text-xl font-bold">Questionários</h1>
       <div className="relative flex place-items-center flex-col pt-4">
-        {/* <Suspense fallback={<p>Carregando...</p>}>
-          <HomeTests></HomeTests>
-        </Suspense> */}
-        <Link
-          href="/take/standalone?demo=true"
-          className="underline hover:text-slate-700"
-        >
-          Demonstração de teste
-        </Link>
+        Verificando questões...
       </div>
     </main>
   );
